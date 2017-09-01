09673c788c1ba64e65d4249af537ab0e68e3674c
theachyutkadam <theachyutkadam@gmail.com>
2017-09-01 22:17:55 +0530

    create scaffolding Customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :customers
   resources :items
   resources :vendors
   # The priority is based upon order of creation: first created -> highest priority.
