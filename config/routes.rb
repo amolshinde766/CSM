2d764b1a4f133c77b5f31040e4ef2d0df9b3b365
ravikirankadam <ravikirankadam.1991@gmail.com>
2017-09-03 00:11:08 +0530

    add dashboard page as vendors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'vendors#index'
   resources :orders
   resources :customers
   resources :items
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
